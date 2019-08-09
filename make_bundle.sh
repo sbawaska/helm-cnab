507ff2262c2352a71f59e406f489d56116ad7e1e
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-08-08 23:43:20 -0700

    make sed platform independent

--- make_bundle.sh
+++ make_bundle.sh
@@ -7,9 +7,13 @@ set -o pipefail
 chart_name=$1
 chart_url=$2
 
-mv cnab/Dockerfile Dockerfile.tmp
-sed -i "s/chart_name/${chart_name}/g" Dockerfile.tmp
-sed -i "s/chart_url/${chart_url}/g" Dockerfile.tmp
+sedi () {
+    sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
+}
+
+cp cnab/Dockerfile Dockerfile.tmp
+sedi "s*chart_name*${chart_name}*g" Dockerfile.tmp
+sedi "s*chart_url*${chart_url}*g" Dockerfile.tmp
 mv Dockerfile.tmp cnab/Dockerfile
 
 cat duffle.json | jq ".name = \"${chart_name}\"" > duffle.json.tmp
