84f122e9a3bc7929a9956e32670d1b4afe2a55af
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-08-08 23:18:30 -0700

    update bundle name

--- make_bundle.sh
+++ make_bundle.sh
@@ -11,3 +11,6 @@ mv cnab/Dockerfile Dockerfile.tmp
 sed -i "s/chart_name/${chart_name}/g" Dockerfile.tmp
 sed -i "s/chart_url/${chart_url}/g" Dockerfile.tmp
 mv Dockerfile.tmp cnab/Dockerfile
+
+cat duffle.json | jq ".name = \"${chart_name}\"" > duffle.json.tmp
+mv duffle.json.tmp duffle.json
