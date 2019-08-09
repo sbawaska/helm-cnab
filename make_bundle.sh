1714044ccaa2b85ce6e11fc5b5a0de913cac5fcc
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-08-08 22:49:08 -0700

    script to make a bundle

---
+++ make_bundle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+chart_name=$1
+chart_url=$2
+
+mv cnab/Dockerfile Dockerfile.tmp
+sed -i "s/chart_name/${chart_name}/g" Dockerfile.tmp
+sed -i "s/chart_url/${chart_url}/g" Dockerfile.tmp
+mv Dockerfile.tmp cnab/Dockerfile
